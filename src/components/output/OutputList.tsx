e3005693a355d8bbdc6579fcbdea7ac74006adaa
smozhevsky <smozhevsky@gmail.com>
2021-06-17 13:34:58 +0300

    some changes in Grid's props

--- src/components/output/OutputList.tsx
+++ src/components/output/OutputList.tsx
@@ -4,9 +4,7 @@ import { makeStyles } from "@material-ui/core";
 const useStyles = makeStyles({
   listItems: {
     listStyleType: "none",
-  },
-  listStyle: {
-    border: "1px solid black",
+    textAlign: "center",
   },
 });
 
@@ -14,7 +12,7 @@ export const OutputList = (props: any) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.listStyle}>
+    <div>
       {props.results.map((result: any, index: any) => (
         <ul className={classes.listItems}>
           <li>{`№${index + 1}--${result[0]}/${result[1]}`}</li>

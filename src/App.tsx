f29b36d99c4e53682352663ed0c45c1ed5b2baa8
smozhevsky <smozhevsky@gmail.com>
2021-06-21 23:31:43 +0300

    change some types

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 // import { useMediaQuery } from "@material-ui/core";
 import React, { useState } from "react";
-import AspectForm from "./components/AspectForm";
+import AspectForm, { VALUES } from "./components/AspectForm";
 import DialogModal from "./components/dialog/DialogModal";
 import { getAspectRatio } from "./utils/utils";
 import Output from "./components/Output";
 
+export type Ratio = [number, number];
+
 function App() {
   const [open, setOpen] = useState(false);
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Ratio[]>([]);
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  const handleOnSubmit = ({ width, height, maxWidth }: any) => {
+  const handleOnSubmit = ({ width, height, maxWidth }: VALUES) => {
     setOpen(true);
     setResults(getAspectRatio(+width, +height, +maxWidth));
   };
